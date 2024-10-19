70391018c96c3f05b365ac62d96eced0732d8466
Bao44 <tqbao44@gmail.com>
2024-10-19 19:45:48 +0700

    create UI JFrameASE, JFrameRSA

---
+++ src/main/java/vn/edu/iuh/fit/miniprojectattt/MiniProjectATTT.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vn.edu.iuh.fit.miniprojectattt;
+
+/**
+ * @author Dell
+ */
+public class MiniProjectATTT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
